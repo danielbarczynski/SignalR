040c3614183cd3c066365f066b1d2e0b7915f436
Daniel PC <daniel.barczynski@onet.pl>
2022-06-10 17:52:10 +0200

    group done

--- SignalR/Hubs/MessageHub.cs
+++ SignalR/Hubs/MessageHub.cs
@@ -13,5 +13,38 @@ namespace SignalR.Hubs
         {
             return Clients.All.SendAsync("ReceiveMessage", message); // first parameter is for js method
         }
+
+        public Task SendToClient(string message)
+        {
+            return Clients.Caller.SendAsync("ReceiveMessage", message);
+        }
+
+        public Task SendToUser(string connectionId, string message)
+        {
+            return Clients.Client(connectionId).SendAsync("ReceiveMessage", message);
+        }
+
+
+        public Task JoinGroup(string group)
+        {
+            return Groups.AddToGroupAsync(Context.ConnectionId, group);
+        }
+
+        public Task SendToGroup(string group, string message)
+        {
+            return Clients.Group(group).SendAsync("ReceiveMessage", message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("UserConnected", Context.ConnectionId);
+            await base.OnConnectedAsync();
+        }
+        
+        public override async Task OnDisconnectedAsync(Exception e)
+        {
+            await Clients.All.SendAsync("UserConnected", Context.ConnectionId);
+            await base.OnDisconnectedAsync(e);
+        }
     }
 }
---
+++ SignalR/wwwroot/js/message.js
@@ -0,0 +1,61 @@
+﻿"use strict";
+
+const connection = new signalR.HubConnectionBuilder()
+  .withUrl("/messages")
+  .build();
+
+connection.start().catch(function (err) {
+  return console.error(err.toString());
+});
+
+// sending messages
+
+document
+  .getElementById("sendButton") // <input type="button" id="sendButton" value="Send Message" />
+  .addEventListener("click", function (event) {
+    let message = document.getElementById("message").value; // <textarea name="message" id="message" rows="3"></textarea>
+    connection.invoke("SendMessageToAll", message).catch(function (err) {
+      return console.error(err.toString());
+    });
+    event.preventDefault();
+    document.getElementById("message").value = "";
+  });
+
+document
+  .getElementById("sendToMeButton")
+  .addEventListener("click", function (event) {
+    let message = document.getElementById("message").value;
+    connection.invoke("SendToClient", message).catch(function (err) {
+      return console.error(err.toString());
+    });
+    event.preventDefault();
+    document.getElementById("message").value = "";
+  });
+
+document
+  .getElementById("sendToGroupButton")
+  .addEventListener("click", function (event) {
+    let message = document.getElementById("message").value;
+    connection.invoke("SendToGroup", "group", message).catch(function (err) {
+      return console.error(err.toString());
+    });
+    event.preventDefault();
+    document.getElementById("message").value = "";
+  });
+
+// then receiving
+
+connection.on("ReceiveMessage", function (message) {
+  let div = document.createElement("div");
+  div.textContent = message;
+  document.getElementById("messages").appendChild(div); // <div id-"messages"></div> append dodaje kolejnego diva do listy
+});
+
+document
+  .getElementById("joinGroup")
+  .addEventListener("click", function (event) {
+    connection.invoke("JoinGroup", "group").catch(function (err) {
+      return console.error(err.toString());
+    });
+    event.preventDefault();
+  });
