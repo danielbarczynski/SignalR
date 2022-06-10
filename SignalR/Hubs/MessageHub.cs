d4600ec04e8d062d6f995e39f096fa6fc25a49a2
Daniel PC <daniel.barczynski@onet.pl>
2022-06-10 15:25:43 +0200

    Add project files.

---
+++ SignalR/Hubs/MessageHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.Hubs
+{
+    public class MessageHub : Hub
+    {
+        public Task SendMessageToAll(string message)
+        {
+            return Clients.All.SendAsync("ReceiveMessage", message); // first parameter is for js method
+        }
+    }
+}
---
+++ SignalR/wwwroot/js/messagee.js
@@ -0,0 +1,26 @@
+﻿"use strict";
+
+const connection = new signalR.HubConnectionBuilder()
+  .withUrl("/messages")
+  .build();
+
+connection.on("ReceiveMessage", function (message) {
+  let div = document.createElement("div");
+  div.textContent = message;
+  document.getElementById("messages").appendChild(div); // append dodaje kolejnego diva do list
+});
+
+connection.start().catch(function (err) {
+  return console.error(err.toString());
+});
+
+document
+  .getElementById("sendButton")
+  .addEventListener("click", function (event) {
+    let message = document.getElementById("message").value;
+    connection.invoke("SendMessageToAll", message).catch(function (err) {
+      return console.error(err.toString());
+    });
+    event.preventDefault();
+    document.getElementById("message").value = "";
+  });
